1684b005bd8037bc112e65ebb4229db37154572a
Charles Green <cgreen7@sheffield.ac.uk>
2018-04-12 17:58:58 +0100

    File name and folder change

--- Raspberry Pi Code/pisetup.sh
+++ Raspberry Pi Code/pisetup.sh
@@ -5,8 +5,8 @@ wget http://node-arm.herokuapp.com/node_latest_armhf.deb
 sudo dpkg -i node_latest_armhf.deb
 mkdir fish
 cd fish
-npm install --save nodemon
-npm install --save socket.io
-npm install --save express
+sudo npm install -g nodemon
+sudo npm install --save socket.io
+sudo npm install --save express
 sudo apt-get install pigpio
-npm install pigpio
+sudo npm install pigpio
