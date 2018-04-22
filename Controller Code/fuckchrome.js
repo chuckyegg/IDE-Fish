a43d4a7117d0479a3cddf57092d591eddbd8099d
Charles Green <cgreen7@sheffield.ac.uk>
2018-04-22 23:26:02 +0100

    Implemented reactive xbox controller with predesigned front end

--- Controller Code/fuckchrome.js
+++ Controller Code/fuckchrome.js
@@ -3,8 +3,9 @@ var express = require('express');
 var app = express();
 
 app.listen(3000);
-
+app.use(express.static('public'));
 app.get('/', (req, res) => {
-	res.sendFile(__dirname + "/gamepad.html");
+	
+	res.sendFile(__dirname + "/public/fishControl.html");
 
 })
